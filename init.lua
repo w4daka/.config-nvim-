758ab00ccf194c528dd73facc279d0c0b85418e9
w4daka <ignorenegativeideology999@gmail.com>
2025-11-16 08:56:11 +0900

    second commit

--- init.lua
+++ init.lua
@@ -1,3 +1,14 @@
+-- use 2-spaces indent
+vim.opt.expandtab = true
+vim.opt.shiftround = true
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
+-- scroll offset as 3 lines
+vim.opt.scrolloff = 3
+-- move the cursor to the previous/next line across the first/last character
+vim.opt.whichwrap = 'b,s,h,l,<,>,[,],~'
+
 vim.api.nvim_create_user_command(
 	'InitLua',
     	function()
@@ -20,4 +31,27 @@ vim.g.clipboard = {
 }
 
 vim.opt.clipboard = 'unnamedplus'
+-- augroup for this config file
+local augroup = vim.api.nvim_create_augroup('init.lua', {})
+
+-- wrapper function to use internal augroup
+local function create_autocmd(event, opts)
+  vim.api.nvim_create_autocmd(event, vim.tbl_extend('force',{
+    group = augroup,  
+  }, opts))
+end
+-- https://vim-jp.org/vim-users-jp/2011/02/20/Hack-202.html
+create_autocmd('BufWritePre',{
+  pattern = '*',
+  callback = function(event) 
+    local dir = vim.fs.dirname(event.file)
+    local force = vim.v.cmdbang == 1
+    if vim.fn.isdirectory(dir) == 0
+      and (force or vim.fn.confirm('"' .. dir .. '"dose not exist. Create?', "&Yes\n&No") == 1) then
+      vim.fn.mkdir(vim.fn.iconv(dir, vim.opt.encoding:get(),vim.opt.termencoding:get()), 'p')
+    end
+  end,
+  desc = 'Auto mkdir to save file'
+})
+
 
