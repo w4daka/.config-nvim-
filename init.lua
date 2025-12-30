b6f5be2bd30c8dd7488484b36184bc71a8f03f09
w4daka <ignorenegativeideology999@gmail.com>
2025-12-30 10:14:47 +0900

    lsp.config edit

--- init.lua
+++ init.lua
@@ -329,47 +329,46 @@ now(function()
   vim.lsp.config('*', {
     root_markers = { '.git' },
   })
-  vim.lsp.config('lua_ls', {
-    cmd = { 'lua-language-server' },
-    filetypes = { 'lua' },
-    on_init = function(client)
-      if client.workspace_folders then
-        local path = client.workspace_folders[1].name
-        if path ~= vim.fn.stdpath('config') and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then
-          return
-        end
-      end
-      client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-        runtime = { version = 'LuaJIT' },
-        workspace = {
-          checkThirdParty = false,
-          library = vim.list_extend(vim.api.nvim_get_runtime_file('lua', true), {
-            '${3rd}/luv/library',
-            '${3rd}/busted/library',
-          }),
-        }
-      })
-    end,
-    settings = {
-      Lua = {}
-    }
-  })
-	vim.lsp.config('clangs',{
-	cmd = {'clangd'},
-	filetypes = {'c','cpp','objc','objcpp'},
+  -- vim.lsp.config('lua_ls', {
+  --   cmd = { 'lua-language-server' },
+  --   filetypes = { 'lua' },
+  --   on_init = function(client)
 
-	settings = {
-		clangd = {
-		argument = {
-			'--compile-commands-dir=.',
-		 	'--background-index',
-      '--pch-storage=disk',
-			}
-		}
-	}
-})
+  --       if path ~= vim.fn.stdpath('config') and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then
+  --         return
+  --       end
+  --     end
+  --     client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+  --       runtime = { version = 'LuaJIT' },
+  --       workspace = {
+  --         checkThirdParty = false,
+  --         library = vim.list_extend(vim.api.nvim_get_runtime_file('lua', true), {
+  --           '${3rd}/luv/library',
+  --           '${3rd}/busted/library',
+  --         }),
+  --       }
+  --     })
+  --   end,
+  --   settings = {
+  --     Lua = {}
+  --   }
+  -- } )
+-- 	vim.lsp.config('clangs',{
+-- 	cmd = {'clangd'},
+-- 	filetypes = {'c','cpp','objc','objcpp'},
+--
+-- 	settings = {
+-- 		clangd = {
+-- 		argument = {
+-- 			'--compile-commands-dir=.',
+-- 		 	'--background-index',
+--       '--pch-storage=disk',
+-- 			}
+-- 		}
+-- 	}
+-- })
 
-  vim.lsp.enable('lua_ls','clangd')
+  vim.lsp.enable('lua_ls' , 'clangd')
 end)
 later(function()
   require('mini.fuzzy').setup()
